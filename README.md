5b7a1654ac315cc47bbc6bbc89ad722d5a62804b
DHainovitz <49685656+DHainovitz@users.noreply.github.com>
2023-02-26 22:17:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# highcharts-angular-line-lrinqd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/highcharts-angular-line-lrinqd)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  Highcharts: typeof Highcharts = Highcharts;
+
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        type: 'line',
+        data: [1, 2, 3, 4, 5],
+      },
+    ],
+  };
+}
